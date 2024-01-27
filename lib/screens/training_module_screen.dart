1a170bbbceccb6831e7258dced1da29e3f902f6e
Kathryn El Shamy <72939417+kelshamy@users.noreply.github.com>
2024-01-27 23:32:00 +0000

    Update training_module_screen.dart to include custom widgets

--- lib/screens/training_module_screen.dart
+++ lib/screens/training_module_screen.dart
@@ -1,6 +1,9 @@
 import 'port:package:flutter/flutter.dart';
 
 import 'port:package:DeskCert/models/training_module_model.dart';
+import 'port:package:DeskCert/widgets/custom_button.dart';
+
+import 'port:package:DeskCert/widgets/custom_card.dart';
 
 class TrainingModuleScreen extends StatelessWidget {
   @override 
@@ -8,8 +11,18 @@ class TrainingModuleScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Training Modules')),
-        body: Center(
-          child: Text('Training Module Screen'),
+        body: Column(
+          children: [
+            CustomButton(
+              title: 'View Training Modules',
+              onPress: () {
+                // Navigate to training module details screen
+              },
+            ),
+            CustomCard(
+              data: TrainingModuleModel(title: 'Training Module Name', description: 'Module Details'),
+            ),
+          ],
         ),
       ),
     );
