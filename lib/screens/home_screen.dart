fc2451b8b1f417d5493f642a9acb9b925d837780
Kathryn El Shamy <72939417+kelshamy@users.noreply.github.com>
2024-01-27 23:25:32 +0000

    Update home_screen.dart to include custom widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,15 +2,27 @@ import 'port:package:flutter/flutter.dart';
 
 import 'port:package:DeskCert/models/course_model.dart';
 import 'port:package:DeskCert/models/certification_model.dart';
+import 'package:DeskCert/widgets/custom_button.dart';
+import 'package:DeskCert/widgets/custom_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('DeskCert Home Page')),
-        body: Center(
-          child: Text('Welcome to DeskCert!'),
+        body: Column(
+          children: [
+            CustomButton(
+              title: 'View Courses',
+              onPress: () {
+                // Navigate to course catalog screen
+             },
+            ),
+            CustomCard(
+              data: CertificationModel(title: 'Certification', description: 'Details of certification'),
+            ),
+          ],
         ),
       ),
     );
