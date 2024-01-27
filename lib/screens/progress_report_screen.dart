083fddb6d8493fd70deaac1985a221322de5bebc
Kathryn El Shamy <72939417+kelshamy@users.noreply.github.com>
2024-01-27 23:32:59 +0000

    Update progress_report_screen.dart to include custom widgets

--- lib/screens/progress_report_screen.dart
+++ lib/screens/progress_report_screen.dart
@@ -1,6 +1,9 @@
 import 'port:package:flutter/flutter.dart';
 
 import 'port:package:DeskCert/models/progress_report_model.dart';
+import 'port:package:DeskCert/widgets/custom_button.dart';
+
+import 'port:package:DeskCert/widgets/custom_card.dart';
 
 class ProgressReportScreen extends StatelessWidget {
   @override 
@@ -8,8 +11,19 @@ class ProgressReportScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Progress Report')),
-        body: Center(
-          child: Text('Progress Report Screen'),
+        body: Column(
+          children: [
+            CustomButton(
+              title: 'View Progress',
+              onPress: () {
+                // Navigate to progress report details screen
+
+              },
+            ),
+            CustomCard(
+              data: ProgressReportModel(title: 'Progress Report', description: 'Report Details'),
+            ),
+          ],
         ),
       ),
     );
