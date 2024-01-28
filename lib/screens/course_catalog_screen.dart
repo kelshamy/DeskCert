a165196660cfbf23e06c85899b8c9f8a409746ad
Kathryn El Shamy <72939417+kelshamy@users.noreply.github.com>
2024-01-28 11:09:54 +0000

    Update course_catalog_screen.dart to include custom widgets

--- lib/screens/course_catalog_screen.dart
+++ lib/screens/course_catalog_screen.dart
@@ -2,27 +2,22 @@ import 'port:package:flutter/flutter.dart';
 
 import 'port:package:DeskCert/models/course_model.dart';
 import 'port:package:DeskCert/widgets/custom_card.dart';
-import 'port:package:DeskCert/widgets/custom_button.dart';
 
-class CourseCatalogScreen extends StatelessWidget {
+class CourseCatalogSCreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text('Course Catalog'),
-        body: Column(
-          children: [
-            CustomButton(
-              title: 'View Courses',
-              onPress: () {
-                // Navigate to course details screen
-
-              },
-            ),
-            CustomCard(
-              data: CourseModel(title: 'Course Name', description: 'Course Details'),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Course Catalog'),
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          builder: (context, index) {
+            return CustomCard(
+              data: CourseModel(title: 'Course ${index+1}', description: 'Course details'),
+            );
+          },
         ),
       ),
     );
